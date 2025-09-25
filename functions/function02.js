e974728d5296be6a49895fb13d67774ad344fa21
manvindergill <manvindersinghgill1@gmail.com>
2025-09-25 17:54:44 +0530

    new files

---
+++ functions/function02.js
@@ -0,0 +1,18 @@
+// sayHello()
+// function sayHello(){
+//     console.log("hello");
+    
+// }
+// saymyname()
+// const saymyname = function(){
+//     console.log("say my name");
+    
+// }
+
+let deleteUser;
+
+if (userIsAdmin) {
+    deleteUser = function() {  // This works perfectly!
+        console.log("User deleted");
+    };
+}
